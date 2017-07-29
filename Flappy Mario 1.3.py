cfd176ae72493f502ddff665dd74eac9d5e29038
Gabriel Antao <gb.antao@gmail.com>
2017-07-29 17:57:01 -0300

    Primeira modulacao de dificuldade feita

--- Flappy Mario 1.3.py
+++ Flappy Mario 1.3.py
@@ -13,7 +13,6 @@ import sys
 import random
 import pygame
 from pygame.locals import *
-###from vector2d import *
 pygame.init()
 
 WIDTH = 1000
@@ -23,7 +22,6 @@ BLACK = (0,0,0)
 GND_HEIGHT = 80 # altura do chao
 # TODO: alterar esses valoes para modular a dificuldade
 GAP_WIDTH = 300 # distancia entre dois tubos 
-GAP_HEIGHT = 120 # espaco vertical para que o sprite possa passar
 
 # cria o clock
 clock = pygame.time.Clock()
@@ -46,6 +44,9 @@ medals = pygame.image.load("images/medals.png")
 mario_sprite = pygame.image.load("images/mario_sprite.png")
 signature = pygame.image.load("images/signature.png")
 pipe_img = pygame.image.load("images/pipe.png")
+level_img = pygame.image.load("images/level.png")
+cursor_img = pygame.image.load("images/cursor.png")
+play_again = pygame.image.load("images/play_again.png")
 
 large_font = pygame.font.Font("fonts/Super_Mario_World.ttf", 32)
 small_font = pygame.font.Font("fonts/Super_Mario_World.ttf", 20)
@@ -70,20 +71,27 @@ class Game:
         self.ground_pos = 0
         self.mario_rect = pygame.Rect(50,200,40,40)  
         self.mario_speed = 0  
+        self.level = 0 
         
-    # cria os rects dos tubos
+    # controla criacao e remocao dos rects dos tubos
+    # obs: modula dificuldade
     def create_pipe(self):
+        if self.level == 0:
+            gap_width = 400
+            gap_height = 200 
+        elif self.level == 1:
+            gap_width = 300
+            gap_height = 140
+        elif self.level == 2:
+            gap_width = random.randrange(250, 401, 50)
+            gap_height = 140
+        elif self.level == 3:
+            gap_width = random.randrange(200, 301, 50)
+            gap_height = random.randrange(120, 201, 40)
         width = pipe_img.get_width()
-        height = random.randrange(0, HEIGHT - GAP_HEIGHT, 20)
-        if height == 0:
-            up_pipe = None
-            down_pipe = pygame.Rect(WIDTH, height + GAP_HEIGHT, width - 4, HEIGHT - height - GAP_HEIGHT - GND_HEIGHT)
-        elif 0 < height < (HEIGHT - GAP_HEIGHT):
-            up_pipe = pygame.Rect(WIDTH, 0, width - 4, height)
-            down_pipe = pygame.Rect(WIDTH, height + GAP_HEIGHT, width - 4, HEIGHT - height - GAP_HEIGHT - GND_HEIGHT)
-        elif height == (HEIGHT - GAP_HEIGHT):
-            up_pipe = pygame.Rect(WIDTH, 0, width - 4, height)
-            down_pipe = None
+        height = random.randrange(0, HEIGHT - gap_height, 20)
+        up_pipe = pygame.Rect(WIDTH, 0, width, height)
+        down_pipe = pygame.Rect(WIDTH, height + gap_height, width, HEIGHT - height - gap_height - GND_HEIGHT)
         self.pipe_list.append([up_pipe, down_pipe])
     
     # movimenta os tubos
@@ -157,6 +165,8 @@ class Game:
         
     def start(self):
         hammer_counter = 0
+        cursor1_pos = 0
+        cursor2_pos = 0
         while True:
             clock.tick(30)
             screen.fill(WHITE)
@@ -167,21 +177,33 @@ class Game:
                 if event.type == KEYDOWN:
                     # Gerencia a troca de modos de jogo quando o espaco eh apertado
                     if event.key == K_SPACE:
-                        if self.game_mode   == 1:
-                            self.game_mode = 2
-                        elif self.game_mode == 2:
+                        if self.game_mode == 2:
                             self.game_mode = 3
                         elif self.game_mode == 3:
-                            self.mario_speed -= 14
+                            self.mario_speed -= 13
                     # Gerencia a troca de modos de jogo quando o enter eh apertado
                     if event.key == K_RETURN or event.key == K_KP_ENTER :
                         if self.game_mode == 1:
+                            self.level = cursor1_pos
                             self.game_mode = 2
-                        elif self.game_mode == 2:
-                            self.game_mode = 3
                         elif self.game_mode == 4:
-                            self.game_mode = 1
+                            if cursor2_pos == 0: #PLAY AGAIN? <YES>
+                                self.game_mode = 2
+                            else: #PLAY AGAIN? <NO>
+                                self.game_mode = 1
+                                cursor2_pos = 0
                             self.reset()
+                    if event.key == K_UP:
+                        if self.game_mode == 1 and cursor1_pos > 0:
+                            cursor1_pos -= 1
+                        if self.game_mode == 4 and cursor2_pos > 0:
+                            cursor2_pos -= 1
+                    if event.key == K_DOWN:
+                        if self.game_mode == 1 and cursor1_pos < 3:
+                            cursor1_pos += 1
+                        if self.game_mode == 4 and cursor2_pos < 1:
+                            cursor2_pos += 1
+                        
         #-----------------------------------------------------------------------
         # GERENCIA OS MODOS DE JOGO
         # 1 modo inicial; 2 preparacao para o jogo; 3 rodando jogo; 4 game over
@@ -191,11 +213,12 @@ class Game:
                 self.draw_parallax()
                 self.draw_ground()
                 self.scroll_ground()              
-                # desenha o titulo, o botao e assinatura
+                # desenha o titulo, level e assinatura
                 screen.blit(game_title,  (50, 50))
-                screen.blit(start_button, (WIDTH/2 - 50, 400))
+                screen.blit(level_img, (400,300))
                 screen.blit(signature, (250, 550))
-        
+                screen.blit(cursor_img, (370, 300 + 25*cursor1_pos)) 
+           
             # MODO DE JOGO 2: PREPARACAO (GET READY)
             elif self.game_mode == 2:
                 self.draw_parallax()
@@ -204,9 +227,9 @@ class Game:
                 self.scroll_ground()
                 # desenha instrucoes
                 screen.blit(get_ready,  (300, 100))
-                # desenha animacao do mario                                   
+                # desenha animacao do mario
+                # TODO: ajustar o tempo da animacao do martelo
                 if hammer_counter > 25:
-                    ###pygame.draw.rect(screen, (255,0,0), (450,410,90,20), 4)
                     screen.blit(crush,  (430, 370))
                     hammer_counter = 0
                 screen.blit(mario_hammer,  (420, 390), (60*(hammer_counter//5), 0, 60, 100))
@@ -215,6 +238,7 @@ class Game:
             # MODO DE JOGO 3: RODANDO O JOGO EM SI
             elif self.game_mode == 3:
                 # TODO: AQUI MUDAR OS VALORES DAS DISTANCIAS PARA MODULAR A DIFICULDADE
+                # TODO: ajustar os criterios as medidas de como adiciona os tubos 
                 # gera obstaculos 
                 if len(self.pipe_list) == 0 or self.pipe_list[-1][0].x == (WIDTH - GAP_WIDTH):
                     self.create_pipe()
@@ -252,7 +276,7 @@ class Game:
                     # para o personagem no chao
                     self.mario_speed = 0
                     self.mario_rect.bottom = HEIGHT - GND_HEIGHT
-                    # desenha o score
+                    # desenha o placar
                     screen.blit(score_gameover,  (WIDTH/2 - 200, 100))
                     # mostra o score e o best score
                     text = large_font.render(str(self.score), True, BLACK)
@@ -261,8 +285,8 @@ class Game:
                         self.best_score = self.score
                     text = large_font.render(str(self.best_score), True, BLACK)
                     screen.blit(text, (600, 290))
-                    text = small_font.render("press enter to play again", True, BLACK)
-                    screen.blit(text, (300, 550))
+                   ### text = small_font.render("press enter to play again", True, BLACK)
+                   ### screen.blit(text, (300, 550))
                     # mostra medalha correspondente 20, 40, 60, 80
                     # TODO: mecher na modulacao da dificuldade (sistema de recompensa)
                     if self.score > 20 : #bronze
@@ -273,6 +297,9 @@ class Game:
                         screen.blit(medals, (340, 225), (160,0,80,100))
                     if self.score > 80: #diamante
                         screen.blit(medals, (340, 225), (240,0,80,100))
+                    # desenha o play again
+                    screen.blit(play_again, (300, 400))
+                    screen.blit(cursor_img, (580, 405 + 30*cursor2_pos))
                 else:
                     self.mario_speed += gravity
                     self.mario_rect.top += self.mario_speed
